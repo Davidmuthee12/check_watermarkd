8ce3244e5c2f3b89eb1426368cfbd382577574a0
unknown <mutheedavid12@gmail.com>
2026-07-02 18:06:49 +0300

    feat: initialize redis and OCR

--- app/main.py
+++ app/main.py
@@ -1,19 +1,37 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
+
 from app.api.router import master_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    # initialize OCR model later
+    # initialize redis later
+
+    yield
+
+    # cleanup if necessary
+
+
+app = FastAPI(
+    title="Watermark Detector",
+    lifespan=lifespan,
+)
+
 app.include_router(master_router)
 
 
 @app.get("/")
-def root():
+async def root():
     return {"message": "welcome to check_watermarkd"}
 
 
-### Scalar Documentation entry point
 @app.get("/scalar", include_in_schema=False)
-def get_scalar_docs():
+async def scalar():
     return get_scalar_api_reference(
         openapi_url=app.openapi_url,
         title="Scalar API",
