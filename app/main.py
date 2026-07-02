6a8eb5a92356d0050acfce95fe467de1b90c8911
unknown <mutheedavid12@gmail.com>
2026-07-02 15:33:38 +0300

    feat(api): adds api main entry point

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
-from fastapi import APIRouter
+from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
+from app.api.router import master_router
 
-app = APIRouter()
+app = FastAPI()
+app.include_router(master_router)
+
+
+@app.get("/")
+def root():
+    return {"message": "welcome to check_watermarkd"}
+
+
+### Scalar Documentation entry point
+@app.get("/scalar", include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API",
+    )
