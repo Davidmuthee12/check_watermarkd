067fe1f2352f25aa83db2c06243eaba47a65447a
unknown <mutheedavid12@gmail.com>
2026-07-02 15:55:20 +0300

    feat(schema): setup api schema for endpoint

--- app/api/routers/video.py
+++ app/api/routers/video.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
+from ..schemas.video import Video
 
 router = APIRouter()
 
 
 ### Upload a watermarked video
 @router.post("/video")
-async def check_watermark():
+async def check_watermark(video: Video):
     pass
